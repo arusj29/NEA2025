0e8a2a70d33cbcb667ffdce5d92e980edfa980a4
Arusjan <19atham@wcgschool.co.uk>
2025-08-29 15:43:21 +0100

    Button
    Added the button class

--- Start.py
+++ Start.py
@@ -1,4 +1,5 @@
 import pygame
+import button
 
 pygame.init()
 
@@ -11,12 +12,30 @@ pygame.display.set_caption("Start Screen")
 font = pygame.font.SysFont("Roboto",60)
 title = font.render("EscapeRoute",0,(0,0,0))
 
+#Button images
+startImg = pygame.image.load('startBtn.png').convert_alpha()
+settingsImg = pygame.image.load('settingsBtn.png').convert_alpha()
+exitImg = pygame.image.load('exitBtn.png').convert_alpha()
+
+
+
+#Button instances
+startButton = button.Button(250,200,startImg)
+settingsButton = button.Button(250,300,settingsImg)
+exitButton = button.Button(250,400,exitImg)
+
 
 run = True   
 while run:
     screen.fill((255,255,255)) #White background
-    screen.blit(title,(280,100))
+    screen.blit(title,(250,100))
     
+    if startButton.draw(screen):
+        print("Start")
+    if settingsButton.draw(screen):
+        print("Settings")
+    if exitButton.draw(screen):
+        run = False
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
