f76b33aff8f02d5b07d52ac257d66ad72cb50e64
Arusjan <19atham@wcgschool.co.uk>
2025-08-29 13:00:53 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -6,11 +6,25 @@ screenWidth = 800   #Holds the value for the width of the screen
 screenHeight = 600  #Holds the value for the height of the screen
 
 screen = pygame.display.set_mode((screenWidth,screenHeight))
+pygame.display.set_caption("Start Screen")
 
-run = True   #
+font = pygame.font.SysFont("Roboto",60)
+title = font.render("EscapeRoute",0,(0,0,0))
+
+
+run = True   
 while run:
+    screen.fill((255,255,255)) #White background
+    screen.blit(title,(280,100))
+    
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    
+
 pygame.quit() 
+
+
+
